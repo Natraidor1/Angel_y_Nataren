feab7f1b62c0868615772f50f4b691c784a29d50
JoseIraheta234 <Jluisiraheta097@gmail.com>
2024-05-07 10:03:00 -0600

    Agregue el rcv

--- app/src/main/java/modelo/claseConexion.kt
+++ app/src/main/java/modelo/claseConexion.kt
@@ -7,7 +7,7 @@ class claseConexion {
 
     fun cadenaDeConexion(): Connection?{
         try {
-            val ip = "jdbc:oracle:thin:@10.10.0.146:1521:xe"
+            val ip = "jdbc:oracle:thin:@10.10.1.166:1521:xe"
             val usuario = "system"
             val contrasena = "desarrollo"
 
