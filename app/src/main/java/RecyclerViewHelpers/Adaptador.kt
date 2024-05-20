e8de8c54098ad6a483091a5631be7784546fc4f3
German <gonzagerman924@gmail.com>
2024-05-20 15:42:41 -0600

    casi finalizado el card

--- app/src/main/java/RecyclerViewHelpers/Adaptador.kt
+++ app/src/main/java/RecyclerViewHelpers/Adaptador.kt
@@ -1,7 +1,21 @@
 package RecyclerViewHelpers
 
-class Adaptador {
+import DataClassMusica
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
+class Adaptador(var Datos: List<DataClassMusica>): RecyclerView.Adapter<ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 
 
 }
